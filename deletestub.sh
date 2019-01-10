4abc59ef1afc8cfb904b46390593423384ac63cc
mikekaczmarski <mkaczman@gmail.com>
2019-01-10 12:28:17 -0700

    updated scripts

--- deletestub.sh
+++ deletestub.sh
@@ -2,7 +2,7 @@
 
 #
 # Deactivate the stock quote service stb
-# delete the stub deom the cluster
+# delete the stub from the cluster
 #
 kubectl apply -f stock-quote-svc.yaml
-
+kubectl delete deploy quote-stub
--- deploystub.sh
+++ deploystub.sh
@@ -3,4 +3,5 @@
 #
 # Deploy and actovate the stock quote virtual service stub
 #
-kubectl apply -f quote-stub-svc.yaml
+kubectl apply -f quote-stub-pod.yaml
+
