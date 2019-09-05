28473bab2917fe1b30d0c435de328eb3347b94cb
mikekaczmarski <mkaczman@gmail.com>
2019-09-05 10:56:49 -0700

    Updated to run in right directory

--- runvirtualservice.sh
+++ runvirtualservice.sh
@@ -8,11 +8,11 @@ LOOPS=100000
 
 echo "Working Directory is " $WORKDIR 
 
-cd $WORKDIR
-
 while [ $LOOPS -gt 0 ]
 do
   export DISPLAY=
-  /IntegrationTester/RunTests -project $WORKDIR/Project/stockquotestub.ghp  -environment ubuntu -noHTTP -run 3293aa9f:167a43a6e8e:-7c32 -environmentTags env
+  /IntegrationTester/RunTests -project $WORKDIR/Project/stockquotestub.ghp \
+    -data $WORKDIR -configuration $WORKDIR -environment ubuntu -noHTTP \
+    -run 3293aa9f:167a43a6e8e:-7c32 -environmentTags env
   sleep 10
 done
