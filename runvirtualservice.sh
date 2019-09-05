073a4fb0414df9ac4a73a5cfcb5ced4193d3856b
mikekaczmarski <mkaczman@gmail.com>
2019-09-05 10:40:45 -0700

    Added -configuration parameter to stub execution

--- runvirtualservice.sh
+++ runvirtualservice.sh
@@ -11,6 +11,6 @@ echo "Working Directory is " $WORKDIR
 while [ $LOOPS -gt 0 ]
 do
   export DISPLAY=
-  /IntegrationTester/RunTests -project $WORKDIR/Project/stockquotestub.ghp -environment ubuntu -noHTTP -run 3293aa9f:167a43a6e8e:-7c32 -environmentTags env
+  /IntegrationTester/RunTests -project $WORKDIR/Project/stockquotestub.ghp -configuration $WORKDIR -environment ubuntu -noHTTP -run 3293aa9f:167a43a6e8e:-7c32 -environmentTags env
   sleep 10
 done
